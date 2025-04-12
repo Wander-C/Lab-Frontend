68f2c2e3dbc7257f6c09d3b7ab6754ce2bf1a17a
231830068 <3359407338@qq.com>
2025-04-12 20:55:25 +0800

    修改role

--- src/untils/index.ts
+++ src/untils/index.ts
@@ -1,8 +1,8 @@
 // 将身份转化为中文显示
 export function parseRole(role: string | null) {
-    if (role === 'CUSTOMER') {
+    if (role === 'user') {
         return '顾客';
-    } else if (role === 'MANAGER') {
+    } else if (role === 'admin') {
         return '管理员';
     }
 }
