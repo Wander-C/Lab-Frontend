3465a359cdb1a45c366496b3e393a72134228118
231830068 <3359407338@qq.com>
2025-04-12 20:20:09 +0800

    allProduct界面

--- src/views/products/AllProduct.vue
+++ src/views/products/AllProduct.vue
@@ -1,54 +1,41 @@
 <script setup lang="ts">
 import { ref } from "vue";
 import ProductItem from "../../components/ProductItem.vue";
+import {getAllProduct} from "../../api/products.ts";
 
-const productList = ref([
+const productList1 = ref([
     {
         productId: 1,
         productTitle: "Product 1",
         productPrice: 100,
-        productCover: "",
+        productCover: "http://dyy-1.oss-cn-nanjing.aliyuncs.com/t01b0ee34ea4870e36a_%E5%89%AF%E6%9C%AC.jpg",
+        productDescribe: "Product 1 Describe",
+        productDetail: "Product 1 Detail",
+        productRate: 5,
     },{
         productId: 2,
         productTitle: "Product 2",
         productPrice: 200,
-        productCover: "",
-    },{
-        productId: 3,
-        productTitle: "Product 3",
-        productPrice: 300,
-        productCover: "",
-    }, {
-        productId: 4,
-        productTitle: "Product 4",
-        productPrice: 400,
-        productCover: "",
-    }, {
-        productId: 5,
-        productTitle: "Product 5",
-        productPrice: 500,
-        productCover: "",
-    }, {
-        productId: 6,
-        productTitle: "Product 6",
-        productPrice: 600,
-        productCover: "",
-    }, {
-        productId: 7,
-        productTitle: "Product 7",
-        productPrice: 700,
-        productCover: "",
-  }
-
+        productCover: "http://dyy-1.oss-cn-nanjing.aliyuncs.com/t01b0ee34ea4870e36a_%E5%89%AF%E6%9C%AC.jpg",
+        productDescribe: "Product 2 Describe",
+        productDetail: "Product 2 Detail",
+        productRate: 4.5,
+    }
 ])
+
+const productList = ref([])
+getAllProduct().then(res => {
+    productList.value = res.data.data
+})
+
 </script>
 
 <template>
   <div class="allProduct">
     <ProductItem
         v-for="product in productList"
-        :key="product.productId"
         :product="product"
+        :key="product.id"
 
     />
   </div>
