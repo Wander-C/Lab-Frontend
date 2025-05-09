07b274f5872625cbd10925bfe75801a78b903720
231830068 <3359407338@qq.com>
2025-05-09 21:54:57 +0800

    调整按钮

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -52,10 +52,10 @@ function handCommand(command: string) {
         <el-tag class="role-tag" size="large">{{ parseRole(role) }}版</el-tag>
       </el-col>
 
-      <el-col :span="14">
+      <el-col :span="15">
       </el-col>
 
-      <el-col :span="1" class="header-icon">
+      <el-col :span="1" class="header-icon" v-if="parseRole(role) === '管理员'">
         <el-dropdown placement="bottom" @command="handCommand">
           <el-icon :size="35" color="white">
             <el-icon-plus />
@@ -88,7 +88,7 @@ function handCommand(command: string) {
         </router-link>
       </el-col>
 
-      <el-col :span="1" class="header-icon">
+      <el-col :span="1" class="header-icon" v-if="parseRole(role) === '顾客'">
         <router-link to="/cart" v-slot="{ navigate }">
           <el-icon @click="navigate" :size="35" color="white">
             <el-tooltip content="购物车" placement="bottom">
