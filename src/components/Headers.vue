3ae60a9cb81e8a47aa11cd8c04560935eed23d48
231830068 <3359407338@qq.com>
2025-05-09 21:56:34 +0800

    调整按钮

--- src/components/Headers.vue
+++ src/components/Headers.vue
@@ -88,7 +88,7 @@ function handCommand(command: string) {
         </router-link>
       </el-col>
 
-      <el-col :span="1" class="header-icon" v-if="parseRole(role) === '顾客'">
+      <el-col :span="1" class="header-icon"  v-if="parseRole(role) === '顾客'">
         <router-link to="/cart" v-slot="{ navigate }">
           <el-icon @click="navigate" :size="35" color="white">
             <el-tooltip content="购物车" placement="bottom">
